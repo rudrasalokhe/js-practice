270fb46b5e706c601943a744c72087cfca4a2f7c
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-01-20 00:01:56 +0530

    number to string

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,11 +1,17 @@
-let score = "33"
-console.log(typeof(score))
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
+let score = "33";
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
 
 
 // number to boolean
 let isLoggedIn = true
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof(booleanIsLoggedIn))  
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+
+
+//number to string
+let someNumber = 22;
+console.log("type of the 22 value variable is:"+typeof(someNumber));
+let stringNumber = String(someNumber);
