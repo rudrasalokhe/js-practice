918407a0f3a4df546d5386f7d07a8fdf2cb3905b
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-02-13 20:05:34 +0530

    coding without refrence

---
+++ exampletodo.js
@@ -0,0 +1,15 @@
+let ctr = 1;
+function addtodo(){
+    const input = document.getElementById('input');
+    const inel = input.value;
+    const divElement = document.createElement('div');
+    divElement.setAttribute('id', ctr);
+    divElement.innerHTML=`<div>${inel}</div><button onclick=deletetodo(${ctr})>Delete</button>`;
+    document.querySelector('body').appendChild(divElement);
+}
+function deletetodo(index){
+    const element = document.getElementById(index);
+    if (element) {
+        element.parentNode.removeChild(element);
+    }
+}
