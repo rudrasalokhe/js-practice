97e936481eaf8590dd050db55fd13dc537ac3a5e
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-02-21 19:18:02 +0530

    params

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const app = express();
-app.get('/sum', function(req, res){
+app.get('/sum/:a/:b', function(req, res){
     const a  = parseInt(req.query.a);
     const b = parseInt(req.query.b);
     res.json({
         answer : a + b
     })
 })
-app.get('/sub', function(req, res){
-    const a  = req.query.a;
-    const b = req.query.b;
+app.get('/sub/:a/:b', function(req, res){
+    const a  = req.params.a;
+    const b = req.params.b;
     res.json({
         answer : a - b
     })
 })
-app.get('/mul', function(req, res){
-    const a  = req.query.a;
-    const b = req.query.b;
+app.get('/mul/:a/:b', function(req, res){
+    const a  = req.params.a;
+    const b = req.params.b;
     res.json({
         answer : a * b
     })
 })
-app.get('/div', function(req, res){
-    const a  = req.query.a;
-    const b = req.query.b;
+app.get('/div/:a/:b', function(req, res){
+    const a  = req.params.a;
+    const b = req.params.b;
     res.json({
         answer : a / b
     })
