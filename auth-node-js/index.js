b95e6d648f6fcc6e2df788d9d3d0b276ae150089
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-02-27 18:23:09 +0530

    27-02-2025

--- auth-node-js/index.js
+++ auth-node-js/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-//middleware express.json for parsing the json data
+// Middleware for parsing JSON data
+app.use(express.json());
 
 const users = [];
 
+// Function to generate a random token
 function generateToken() {
     let options = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -19,46 +21,77 @@ function generateToken() {
     return token;
 }
 
-app.use(express.json());
-
+// Route for user signup
 app.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     users.push({
-        username : username,
-        password : password
-    })
-
+        username: username,
+        password: password
+    });
 
     res.json({
-        message : "You are signed in"
-    })
+        message: "You are signed up!"
+    });
 
     console.log(users);
-})
-app.post('/signin', (req, res) =>{
+});
+
+// Route for user signin
+app.post('/signin', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    let finduser = null;
-    for(let i=0;i<users.length;i++){
-        if(users[i].username == username && users[i].password == password){
-            finduser = users[i];
+
+    let findUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            findUser = users[i];
         }
     }
-    if(finduser){
-        const token = generateToken();
-        finduser.token = token;
-        res.json({
-            token : token
-        })
-    }
-    else{
-        res.status(403).send({
-            message: "Invalid username or password"
-        })
-    }
-    console.log(users);
-})
 
-app.listen(3001);
+    if (findUser) {
+        const token = generateToken();
+        findUser.token = token; // Attach the token to the user object
+        res.json({
+            token: token
+        });
+    } else {
+        res.status(403).json({
+            message: "Invalid username or password"
+        });
+    }
+
+    console.log(users);
+});
+
+
+app.get('/me', (req, res) => {
+    const token = req.headers.authorization;
+
+    let foundUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token === token) {
+            foundUser = users[i];
+            
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password 
+        });
+    } else {
+        res.status(403).json({
+            message: "Invalid token"
+        });
+    }
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+
+
+//1.15.00
