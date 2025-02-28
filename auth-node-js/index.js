4e6eccedd69b3f1b8d67d3028d4e25e46399079d
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-03-01 01:19:46 +0530

    init

--- auth-node-js/index.js
+++ auth-node-js/index.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const app = express();
+const JWT_SECRET = "randomrudrasalokhe";
+const jwt = require('jsonwebtoken');
 
-// Middleware for parsing JSON data
+// this is a middleware which is used to like parse a json data
 app.use(express.json());
 
 const users = [];
 
-// Function to generate a random token
-function generateToken() {
-    let options = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    ];
-
-    let token = '';
-    for (let i = 0; i < 32; i++) {
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
 
 // Route for user signup
 app.post('/signup', (req, res) => {
@@ -51,8 +39,10 @@ app.post('/signin', (req, res) => {
     }
 
     if (findUser) {
-        const token = generateToken();
-        findUser.token = token; // Attach the token to the user object
+        const token = jwt.sign({
+            username : username
+        }, JWT_SECRET);
+        // findUser.token = token; 
         res.json({
             token: token
         });
@@ -67,11 +57,12 @@ app.post('/signin', (req, res) => {
 
 
 app.get('/me', (req, res) => {
-    const token = req.headers.authorization;
-
+    const token = req.headers.token;
+    const decodedInformation = jwt.verify(token, JWT_SECRET);
+    const username = decodedInformation.username;
     let foundUser = null;
     for (let i = 0; i < users.length; i++) {
-        if (users[i].token === token) {
+        if (users[i].username === username) {
             foundUser = users[i];
             
         }
