5ed19a1ab14b4d448d21f3dc0f20443a751ae5a1
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-03-11 00:10:36 +0530

    init

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
 const jwt = require('jsonwebtoken');
+
 const JWT_SECRET = "rudraiscoding";
 app.use(express.json());
+
 let users = [];
 
+// Signup route
 app.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -15,6 +17,7 @@ app.post('/signup', (req, res) => {
         username: username,
         password: password
     });
+
     res.json({
         message: "Signed up"
     });
@@ -22,57 +25,44 @@ app.post('/signup', (req, res) => {
     console.log(users);
 });
 
+// Signin route
 app.post('/signin', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     let userfound = null;
+
     for (let i = 0; i < users.length; i++) {
         if (users[i].username == username && users[i].password == password) {
             userfound = users[i];
         }
     }
-    if (userfound) {
-        const token = jwt.sign({
-            username: username
-        }, JWT_SECRET);
 
-        res.json({
-            token: token
-        });
+    if (userfound) {
+        const token = jwt.sign({ username: username }, JWT_SECRET);
+        res.json({ token: token });
     } else {
-        res.status(401).json({
-            message: "Invalid username and password"
-        });
+        res.status(401).json({ message: "Invalid username and password" });
     }
     console.log(users);
 });
 
+// Authentication middleware
 function auth(req, res, next) {
     const token = req.headers.token;
-    if (!token) {
-        return res.json({
-            message: "You have not logged in"
-        });
-    }
+
     try {
         const decodedInformation = jwt.verify(token, JWT_SECRET);
-        if (decodedInformation.username) {
-            req.username = decodedInformation.username;
-            return next();
-        } else {
-            return res.json({
-                message: "You have not logged in"
-            });
-        }
+        req.username = decodedInformation.username;
+        next();
     } catch (err) {
-        return res.json({
-            message: "Invalid token"
-        });
+        res.json({ message: "Invalid token" });
     }
 }
 
+// Get user information
 app.get('/me', auth, (req, res) => {
     let found = null;
+
     for (let i = 0; i < users.length; i++) {
         if (users[i].username === req.username) {
             found = users[i];
@@ -85,17 +75,17 @@ app.get('/me', auth, (req, res) => {
             password: found.password
         });
     } else {
-        res.status(404).json({
-            message: "User not found"
-        });
+        res.status(404).json({ message: "User not found" });
     }
     console.log(users);
 });
 
+// Serve the HTML file from the `/` route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Start the server
 app.listen(3003, () => {
     console.log("Server is running on port 3003");
 });
--- fullstack/public/script.js
+++
@@ -1,8 +0,0 @@
-async function signup(){
-    const username = document.getElementById('uid').value;
-    const password = document.getElementById('pass').value;
-    await axios.post("http://localhost:3002/", {
-        username : username, 
-        password : password
-    })
-}
